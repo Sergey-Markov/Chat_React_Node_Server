71cad89c8aacb9d0427c4a3cf77a22b09010be00
Sergey-Markov <1nativefl@gmail.com>
2021-12-08 09:21:12 +0200

    AT start npx express-generator --view=ejs .

---
+++ bin/server.js
@@ -0,0 +1,86 @@
+const app = require("../app");
+const debug = require("debug")("chat-react-node-server:server");
+const http = require("http");
+const server = http.createServer(app);
+require("dotenv").config();
+const io = require("socket.io")(server);
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+console.log(port);
+/**
+ * Create HTTP server.
+ */
+io.on("connection", (socket) => {
+  console.log("User connected!");
+  socket.emit("message", "User connected!");
+});
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+io.on("connection", (socket) => {
+  console.log("User connected!");
+  socket.emit("message", "User connected!");
+});
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
+}
+
+const users = {};
